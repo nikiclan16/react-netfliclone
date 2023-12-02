69007f3777abbf611845572db42338f17e17dbf1
Nicolas Castaneda <vladistar200304@gmail.com>
2023-12-02 18:58:49 -0500

    add padding rows

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,80 +1,76 @@
-import { makeStyles, Typography } from '@material-ui/core';
-import React, { useEffect, useState } from 'react'
-import instance from '../axios';
+import { makeStyles, Typography } from "@material-ui/core";
+import React, { useEffect, useState } from "react";
+import instance from "../axios";
 
-export const Row = ({title,fetchUrl,isLargeRow}) => {
-    const classes = useStyles();
+export const Row = ({ title, fetchUrl, isLargeRow }) => {
+  const classes = useStyles();
 
-    const [movies, setMovies] = useState([]);
+  const [movies, setMovies] = useState([]);
 
-    const base_url = 'https://image.tmdb.org/t/p/original/';
+  const base_url = "https://image.tmdb.org/t/p/original/";
 
-    useEffect(() => {
-      
-      const fetchData = async () => {
-        const request = await instance.get(fetchUrl);
-        setMovies(request.data.results);
-        return request;
-      };
+  useEffect(() => {
+    const fetchData = async () => {
+      const request = await instance.get(fetchUrl);
+      setMovies(request.data.results);
+      return request;
+    };
 
-      fetchData();
+    fetchData();
+  }, [fetchUrl]);
 
-    }, [fetchUrl])
-    
-    
   return (
     <div className={classes.root}>
-      <Typography variant='h4'>{title}</Typography>
+      <Typography variant="h4">{title}</Typography>
       <div className={classes.posters}>
         {movies.map(
           (movie) =>
             ((isLargeRow && movie.poster_path) ||
               (!isLargeRow && movie.backdrop_path)) && (
-                <img
-                  className={`${classes.poster} ${
-                    isLargeRow && classes.posterLarge
-                  }`}
-                  key={movie.id}
-                  src={`${base_url}${
-                    isLargeRow ? movie.poster_path : movie?.backdrop_path
-                  }`}
-                  alt={movie?.name}
-                />
-              ) 
+              <img
+                className={`${classes.poster} ${
+                  isLargeRow && classes.posterLarge
+                }`}
+                key={movie.id}
+                src={`${base_url}${
+                  isLargeRow ? movie.poster_path : movie?.backdrop_path
+                }`}
+                alt={movie?.name}
+              />
             )
-        }
+        )}
       </div>
     </div>
   );
 };
 
-
 const useStyles = makeStyles((theme) => ({
   root: {
-    color: '#fff',
+    color: "#fff",
     marginLeft: theme.spacing(4),
+    paddingBottom: "60px",
   },
   posters: {
-    display: 'flex',
-    overflowY: 'hidden',
-    overflowX: 'scroll',
+    display: "flex",
+    overflowY: "hidden",
+    overflowX: "scroll",
     "&::-webkit-scrollbar": {
-      display: 'none',
+      display: "none",
     },
   },
   poster: {
-    maxHeight: '12rem',
-    objectFit: 'contain',
+    maxHeight: "12rem",
+    objectFit: "contain",
     marginRight: theme.spacing(1),
-    transition: 'transform 420ms',
+    transition: "transform 420ms",
     "&:hover": {
       transform: "scale(1.1)",
     },
   },
   posterLarge: {
-    maxHeight: '15rem',
+    maxHeight: "15rem",
     "&:hover": {
-      transform: 'scale(1.15)'
+      transform: "scale(1.15)",
     },
-  }
-}))
+  },
+}));
